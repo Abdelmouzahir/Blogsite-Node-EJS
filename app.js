d3f7a0964fde6a9a0c9be83d57c1943de4058b4b
abdel mouzahir <jalil.mouzahir@gmail.com>
2023-07-25 11:45:03 -0600

    update mongo link

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/blogDB",{useNewUrlParser: true});
+mongoose.connect("mongodb+srv://dembelemaroc:test123@cluster0.1kqmk97.mongodb.net/blogDB",{useNewUrlParser: true});
 
 const postSchema ={
   title: String,
